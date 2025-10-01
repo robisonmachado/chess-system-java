3ce26b11ac76aff31bfac6faeabc3eff169ca9de
Robison Machado <robisonpmachado@gmail.com>
2025-10-01 00:15:45 -0300

    feat: add first class (Position)

---
+++ src/boardgame/Position.java
@@ -0,0 +1,56 @@
+package boardgame;
+
+import java.util.Objects;
+
+public class Position {
+	private int row;
+	private int column;
+	
+	public Position(int row, int column) {
+		this.row = row;
+		this.column = column;
+	}
+
+	public int getRow() {
+		return row;
+	}
+
+	public void setRow(int row) {
+		this.row = row;
+	}
+
+	public int getColumn() {
+		return column;
+	}
+
+	public void setColumn(int column) {
+		this.column = column;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(column, row);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Position other = (Position) obj;
+		return column == other.column && row == other.row;
+	}
+
+	@Override
+	public String toString() {
+		return row + ", " + column;
+	}
+	
+	
+	
+	
+
+}
