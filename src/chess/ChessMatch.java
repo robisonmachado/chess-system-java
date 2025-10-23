3acfc2c33151dce4a9b738ff2ee1dfbab8189a3c
Robison Machado <robisonpmachado@gmail.com>
2025-10-23 01:30:29 -0300

    Implementing possible moves of Rook

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -34,6 +34,12 @@ public class ChessMatch {
 		if(!board.piece(position).isThereAnyPossibleMove()) throw new ChessException("There is no possible moves for the chosen piece");
 	}
 	
+	private void validateTargetPosition(Position source, Position target) {
+				
+		if(!board.piece(source).possibleMove(target)) 
+			throw new ChessException("The chosen piece can't move to target position");
+	}
+	
 	private Piece makeMove(Position source, Position target) {
 		var sourcePiece = board.removePiece(source);
 		var capturedPiece = board.removePiece(target);
@@ -44,10 +50,12 @@ public class ChessMatch {
 	}
 	
 	public ChessPiece performChessMove(ChessPosition sourcePosition, ChessPosition targetPosition) {
-		Position source = sourcePosition.toPosition();
-		Position target = targetPosition.toPosition();
+		var source = sourcePosition.toPosition();
+		var target = targetPosition.toPosition();
 		
 		validateSourcePosition(source);
+		validateTargetPosition(source, target);
+		
 		var capturedPiece = makeMove(source, target);
 		
 		return (ChessPiece) capturedPiece;
--- src/chess/pieces/Rook.java
+++ src/chess/pieces/Rook.java
@@ -1,6 +1,7 @@
 package chess.pieces;
 
 import boardgame.Board;
+import boardgame.Position;
 import chess.ChessPiece;
 import chess.Color;
 
@@ -18,6 +19,55 @@ public class Rook extends ChessPiece{
 	@Override
 	public boolean[][] possibleMoves() {
 		var matriz = new boolean[getBoard().getRows()][getBoard().getColumns()];
+		
+		var auxPosition = new Position(0, 0);
+		
+		// above
+		auxPosition.setValues(position.getRow()-1, position.getColumn());
+		
+		while(getBoard().positionExists(auxPosition) && !getBoard().thereIsAPiece(auxPosition)) {
+			matriz[auxPosition.getRow()][auxPosition.getColumn()] = true;
+			auxPosition.setRow(auxPosition.getRow()-1);
+		}
+		
+		if(getBoard().positionExists(auxPosition) && isThereOpponentPiece(auxPosition)) 
+			matriz[auxPosition.getRow()][auxPosition.getColumn()] = true;
+		
+		// left
+		auxPosition.setValues(position.getRow(), position.getColumn()-1);
+		
+		while(getBoard().positionExists(auxPosition) && !getBoard().thereIsAPiece(auxPosition)) {
+			matriz[auxPosition.getRow()][auxPosition.getColumn()] = true;
+			auxPosition.setColumn(auxPosition.getColumn()-1);
+		}
+		
+		if(getBoard().positionExists(auxPosition) && isThereOpponentPiece(auxPosition)) 
+			matriz[auxPosition.getRow()][auxPosition.getColumn()] = true;
+		
+		// right
+		auxPosition.setValues(position.getRow(), position.getColumn()+1);
+				
+		while(getBoard().positionExists(auxPosition) && !getBoard().thereIsAPiece(auxPosition)) {
+			matriz[auxPosition.getRow()][auxPosition.getColumn()] = true;
+			auxPosition.setColumn(auxPosition.getColumn()+1);
+		}	
+		
+		if(getBoard().positionExists(auxPosition) && isThereOpponentPiece(auxPosition)) 
+			matriz[auxPosition.getRow()][auxPosition.getColumn()] = true;
+		
+		
+		// below
+		auxPosition.setValues(position.getRow()+1, position.getColumn());
+		
+		while(getBoard().positionExists(auxPosition) && !getBoard().thereIsAPiece(auxPosition)) {
+			matriz[auxPosition.getRow()][auxPosition.getColumn()] = true;
+			auxPosition.setRow(auxPosition.getRow()+1);
+		}
+			
+		if(getBoard().positionExists(auxPosition) && isThereOpponentPiece(auxPosition)) 
+			matriz[auxPosition.getRow()][auxPosition.getColumn()] = true;
+		
+		
 		return matriz;
 	}
 
